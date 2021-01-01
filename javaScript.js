8f7fe736dbb7b408365f94ab13368bc12e2d1960
Devin <ospreywebservices@gmail.com>
2021-01-01 17:10:02 -0500

    added slider + some DOM manipulation

--- javaScript.js
+++ javaScript.js
@@ -1,11 +1,20 @@
 const gridContainer = document.querySelector('#gridContainer');
+var rangeSlider = document.getElementById("sliderRange"); 
+var output = document.getElementById("demo"); 
+output.innerHTML = rangeSlider.value + ` x ${rangeSlider.value}`; 
+  
+rangeSlider.oninput = function() {
+    output.innerHTML = `${this.value} x ${this.value}`; 
+} 
 
+
+//add a div child element for given number of rows and cols;
 function makeGrid (rows, cols) {
     gridContainer.style.setProperty('--grid-rows', rows);
     gridContainer.style.setProperty('--grid-cols', cols);
-    for(c = 0; c < (rows * cols); c++) {
+    for(i = 0; i < (rows * cols); i++) {
         let cell = document.createElement('div')
-        gridContainer.appendChild(cell).className = "grid-item"
+        gridContainer.appendChild(cell).className = "grid-cell"
     };
 };
 makeGrid(16, 16);
